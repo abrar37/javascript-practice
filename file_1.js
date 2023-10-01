54fefed99aee606e9205215ee42797b5fabff00a
Abrar A <kh.abrar22@gmail.com>
2023-10-02 00:20:56 +0500

    js basics

---
+++ file_1.js
@@ -0,0 +1,14 @@
+const accountId = 14425  // Can not be changeable
+let accountEmail = "abrar@google.com" //can not be changeable out of scope
+var accountPassword = "124cfw" // changeable out of scope
+
+accountCity = "Lahore"
+
+let accountState;
+accountEmail = "hello@gmail.com"
+
+// console.log(accountId);
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+console.log(typeof accountId);
---
+++ file_2.js
@@ -0,0 +1,81 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello alert") 
+
+// let isLoggedIn = false // boolean
+
+// number => 2
+// bigint
+// string = "Quick"
+// num = standalone value
+// undefined
+// symbol 
+
+// console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(typeof symbol);
+
+// let score = "33"
+let score = false
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 : false => 0 
+
+let isLoggedIn = 0
+let booleanConversion = Boolean(isLoggedIn)
+// console.log(booleanConversion);
+
+// ************** Operations ******************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/3);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(11%2);
+
+let str1 = "Hello"
+let str2 = " Arqam"
+// console.log(str1 + str2);
+// console.log(1 + 2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2  // Never Use Confused code syntex
+// console.log(num1);
+
+let gameCounter = 50;
+// gameCounter++;
+++gameCounter;
+// console.log(gameCounter);
+
+
+console.log("2" == 2);
+console.log("2" === 2); // strict check including data type
+
+
+// console.log(null > 0);
+// console.log(null == 0); // Comparison operator "> or <" treated "null" as a 0 but "==" not treated as 0
+// console.log(null >= 0);
+
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
